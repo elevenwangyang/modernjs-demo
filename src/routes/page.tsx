3d4ef7c107c033b09b43c1ff61b377538259dcbb
wangyang <21@21.com>
2025-08-04 14:47:10 +0800

    update

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from 'rslib-styled-demo';
+import { Button, JsonView } from 'rslib-styled-demo';
 
 const Index = () => (
   <div>
     <Button label="Button" />
+    <JsonView json={{ a: 1 }} />
   </div>
 );
 
